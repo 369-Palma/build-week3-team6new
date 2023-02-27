a09a0a6c55f60c8d719a633291b5907bf479e31a
Giacomo <giax.95@live.it>
2023-02-27 18:07:35 +0100

    Test

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import AziendeConsultate from "./components/AziendeConsultate";
+import UserProfile from "./components/ProfilepageMain";
 
 function App() {
   return (
     <div className="App">
+      <UserProfile></UserProfile>
       <AziendeConsultate></AziendeConsultate>
     </div>
   );
