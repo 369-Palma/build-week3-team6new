f1fc7ec1f0ea98278ab4544acb0bbff50fd060cc
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-27 16:55:37 +0100

    fetch generica su action

--- linkedin/src/components/AziendeConsultate.jsx
+++ linkedin/src/components/AziendeConsultate.jsx
@@ -11,7 +11,7 @@ const AziendeConsultate = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchProfiles());
+    dispatch(fetchProfiles(""));
   }, []);
 
   return (
--- linkedin/src/redux/actions/index.js
+++ linkedin/src/redux/actions/index.js
@@ -1,20 +1,17 @@
 export const GET_USERS = "GET_USERS";
-
-export const fetchProfiles = () => {
+const baseline = "https://striveschool-api.herokuapp.com/api/profile/";
+export const fetchProfiles = (query) => {
   return async (dispatch) => {
     try {
-      const res = await fetch(
-        "https://striveschool-api.herokuapp.com/api/profile/",
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNzAyZGYxOTNlNjAwMTM4MDdmNWEiLCJpYXQiOjE2Nzc0ODgxNzMsImV4cCI6MTY3ODY5Nzc3M30.v4RvPvwPrNqMf1jT8g6IRxX0XpB361UjIv66UzPhULY",
-          },
-        }
-      );
+      const res = await fetch(baseline + query, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNzAyZGYxOTNlNjAwMTM4MDdmNWEiLCJpYXQiOjE2Nzc0ODgxNzMsImV4cCI6MTY3ODY5Nzc3M30.v4RvPvwPrNqMf1jT8g6IRxX0XpB361UjIv66UzPhULY",
+        },
+      });
       if (res.ok) {
         const dataUsers = await res.json();
 
