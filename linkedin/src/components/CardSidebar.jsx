a6acea721d89c63886849c7c591776f9bd609f58
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-27 13:07:13 +0100

    struttura card sidebar

---
+++ linkedin/src/components/CardSidebar.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
+
+const CardSidebar = (props) => {
+  <Card className="d-flex">
+    <Card.Img variant="top" src={props.image} className="fotoTonde" />
+    <Card.Body>
+      <Card.Title>
+        {props.name} + {props.surname}
+      </Card.Title>
+      <Card.Text>{props.title}</Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>;
+};
+
+export default CardSidebar;
