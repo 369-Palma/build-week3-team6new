0dd5a8e52ca21f4a3794355deb4db2a228790a53
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-27 15:27:56 +0100

    sidebar blocco 1 fetch

--- linkedin/src/components/CardSidebar.jsx
+++ linkedin/src/components/CardSidebar.jsx
@@ -2,13 +2,18 @@ import { Button, Card } from "react-bootstrap";
 
 const CardSidebar = (props) => {
   <Card className="d-flex">
-    <Card.Img variant="top" src={props.image} className="fotoTonde" />
+    <Card.Img
+      variant="top"
+      src={props.image}
+      alt={props.name}
+      className="fotoTonde"
+    />
     <Card.Body>
       <Card.Title>
-        {props.name} + {props.surname}
+        {props.name} {props.surname} . <span className="disabled">3°</span>
       </Card.Title>
       <Card.Text>{props.title}</Card.Text>
-      <Button variant="primary">Collegati</Button>
+      <Button className="my-2">Collegati</Button>
     </Card.Body>
   </Card>;
 };
