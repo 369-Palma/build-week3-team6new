cf6c1dfa1a8e8c837130dc0f78ada69118674d75
Giacomo <giax.95@live.it>
2023-02-28 16:42:58 +0100

    Fix immagine.

--- linkedin/src/components/ProfilepageMain.jsx
+++ linkedin/src/components/ProfilepageMain.jsx
@@ -20,17 +20,10 @@ const UserProfile = () => {
       <Card>
         <Container>
           <Row className="p-background">
-            <img
-              src="https://www.e26.it/wp-content/uploads/2018/07/fb-luglio-2018.png"
-              alt=""
-            />
+            <img src="https://www.e26.it/wp-content/uploads/2018/07/fb-luglio-2018.png" alt="" />
           </Row>
           <Row className="profile ml-1">
-            <img
-              className="img-fluid fotoUser"
-              src={profileStore.image}
-              alt="userimage"
-            />
+            <img className="img-fluid fotoUser" src={profileStore.image} alt="userimage" />
           </Row>
           <Row>
             <Col className="userdetail " xs={12} md={8}>
@@ -39,17 +32,10 @@ const UserProfile = () => {
               </h4>
               <p className="my-0 occupation">{profileStore.title}</p>
               <p className="my-0 location text-muted">{profileStore.area}</p>
-              <p className="my-2 connections">
-                580 follower - 951 collegamenti
-              </p>
+              <p className="my-2 connections">580 follower - 951 collegamenti</p>
               <div className="d-flex justify-content-start w-100 mb-3">
-                <Button className="profilebutton open-to-btn">
-                  Disponibile per
-                </Button>
-                <Button
-                  variant="outline-primary"
-                  className="addbtn profilebutton  mx-3"
-                >
+                <Button className="profilebutton open-to-btn">Disponibile per</Button>
+                <Button variant="outline-primary" className="addbtn profilebutton  mx-3">
                   Aggiungi sezione profilo
                 </Button>
                 <Button variant="outline-secondary" className="profile__button">
@@ -59,10 +45,10 @@ const UserProfile = () => {
             </Col>
             <Col xs={12} md={4}>
               <img
-                src="https://yt3.googleusercontent.com/9RGAVa52qAdJfJSShxr9prs7EUpia4tsVsnvrb-E1Yhdj7H4LMU4gHLRDC-7tJLV9BVYaEq6M7g=s900-c-k-c0x00ffffff-no-rj"
+                src="https://reteinformaticalavoro.it/images/company/6013edb782d88_300_300.png"
                 alt=""
                 style={{ width: "3em", height: "3em" }}
-                className="me-2 education mb-1"
+                className="me-2 mb-1"
               />{" "}
               Epicode
             </Col>
