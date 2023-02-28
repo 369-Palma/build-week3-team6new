43dcdb9bc60a05caa52ba5e8f8dc97586d699ffc
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-28 10:11:58 +0100

    Update App.js

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import AziendeConsultate from "./components/AziendeConsultate";
 import UserProfile from "./components/ProfilepageMain";
-
+import { Container, Row, Col } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <UserProfile></UserProfile>
-      <AziendeConsultate></AziendeConsultate>
+      <Container className="d-flex">
+        <Row>
+          <Col className="col-xs-12 col-md-8 w-100">
+            <UserProfile></UserProfile>
+          </Col>
+        </Row>
+        <Col className="d-xs-none d-md-block col-md-4 ms-4">
+          <Row className="my-2">
+            <AziendeConsultate></AziendeConsultate>
+          </Row>
+        </Col>
+      </Container>
     </div>
   );
 }
