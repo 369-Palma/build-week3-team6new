b2ce76a48fe3cfa57183e453c61bc8e1387d88d2
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-03-01 10:39:26 +0100

    added fix to my code and change data in the modal

--- linkedin/src/components/Experiences.jsx
+++ linkedin/src/components/Experiences.jsx
@@ -1,8 +1,11 @@
+
+
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchExp } from "../redux/actions";
-import { HiPlus, HiOutlinePencil, BsFillInfoSquareFill } from "react-icons/hi";
+import { HiPlus, HiOutlinePencil } from "react-icons/hi";
+import { BsFillInfoSquareFill } from "react-icons/bs";
 import { Row, Button, Modal, Form } from "react-bootstrap";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -24,6 +27,49 @@ const Experiences = () => {
 
     return (
         <>
+            <Button variant="primary" onClick={handleShow}>
+                <HiOutlinePencil />
+            </Button>
+
+            <Modal size="lg" show={lgShow} onHide={() => setLgShow(false)} aria-labelledby="example-modal-sizes-title-lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Exp</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Role*</Form.Label>
+                        <Form.Control type="text" autoFocus />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Company*</Form.Label>
+                        <Form.Control type="text" autoFocus />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text" autoFocus />
+                    </Form.Group>
+                    <h4>Posizione attuale</h4>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Area*</Form.Label>
+                        <Form.Control type="text" autoFocus />
+                    </Form.Group>
+                    <h4>Date</h4>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Data Inizio </Form.Label>
+                        <Form.Control type="date" autoFocus />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Data Fine </Form.Label>
+                        <Form.Control type="date" autoFocus />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button type="submit" variant="primary" className="rounded-pill">
+                        Salva
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             <div>
                 <div className="d-flex justify-content-between">
                     <div>
@@ -35,76 +81,22 @@ const Experiences = () => {
                     </div>
                 </div>
 
-                <Modal
-                    size="lg"
-                    show={lgShow}
-                    onHide={() => setLgShow(false)}
-                    aria-labelledby="example-modal-sizes-title-lg"
-                >
-                    <Modal.Header closeButton>
-                        <Modal.Title>Add Exp</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Role*</Form.Label>
-                            <Form.Control type="text" autoFocus />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Company*</Form.Label>
-                            <Form.Control type="text" autoFocus />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control type="text" autoFocus />
-                        </Form.Group>
-                        <h4>Posizione attuale</h4>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Area*</Form.Label>
-                            <Form.Control type="text" autoFocus />
-                        </Form.Group>
-                        <h4>Date</h4>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Data Inizio </Form.Label>
-                            <Form.Control type="date" autoFocus />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Data Fine </Form.Label>
-                            <Form.Control type="date" autoFocus />
-                        </Form.Group>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button type="submit" variant="primary" className="rounded-pill">
-                            Salva
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-                <div>
-                    <div className="d-flex justify-content-between">
-                        <div>
-                            <h3>Esperienza</h3>
-                        </div>
-                        <div>
-                            <HiPlus className="fs-3 me-4" />
-                            <HiOutlinePencil className="fs-4" />
-                        </div>
-                    </div>
-                    <Row className="d-flex text-start">
-                        <h3>{experiences?.role}</h3>
-                        <h4>{experiences?.company}</h4>
-                        <p>{experiences?.description}</p>
-                        <p>{experiences?.startDate}</p>
-                        <p>{experiences?.area}</p>
-                    </Row>
-                    <Row className="d-flex text-start">
-                        <h3>Lavoro</h3>
-                        <h4>Azienda</h4>
-                        <p>date</p>
-                        <p>Luogo</p>
-                    </Row>
-                </div>
+                <Row className="d-flex text-start">
+                    <h3>{experiences?.role}</h3>
+                    <h4>{experiences?.company}</h4>
+                    <p>{experiences?.description}</p>
+                    <p>{experiences?.startDate}</p>
+                    <p>{experiences?.area}</p>
+                </Row>
+                <Row className="d-flex text-start">
+                    <h3>Lavoro</h3>
+                    <h4>Azienda</h4>
+                    <p>date</p>
+                    <p>Luogo</p>
+                </Row>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Experiences
+export default Experiences;
