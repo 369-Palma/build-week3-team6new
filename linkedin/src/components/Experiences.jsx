e0222aac5bc4dedb26915f7db13f4feebcabb5f5
FeliCarlomagno <feliciano.carlomagno@libero.it>
2023-03-01 09:54:10 +0100

    removed conflicts

--- linkedin/src/components/Experiences.jsx
+++ linkedin/src/components/Experiences.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExp } from "../redux/actions";
 import { HiPlus, HiOutlinePencil } from "react-icons/hi";
 import { BsFillInfoSquareFill } from "react-icons/bs";
 import { Row, Button, Modal, Form } from "react-bootstrap";
@@ -5,6 +9,16 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Experiences = () => {
+  ///fetch
+  const dispatch = useDispatch();
+  const experiences = useSelector((state) => state.contentExp[0]);
+
+  useEffect(() => {
+    dispatch(fetchExp("experiences"));
+  }, []);
+
+  ///fetch
+
   const [lgShow, setLgShow] = useState(false);
 
   const handleShow = () => setLgShow(true);
@@ -109,10 +123,11 @@ const Experiences = () => {
         </div>
 
         <Row className="d-flex text-start">
-          <h3>Lavoro</h3>
-          <h4>Azienda</h4>
-          <p>date</p>
-          <p>Luogo</p>
+          <h3>{experiences?.role}</h3>
+          <h4>{experiences?.company}</h4>
+          <p>{experiences?.description}</p>
+          <p>{experiences?.startDate}</p>
+          <p>{experiences?.area}</p>
         </Row>
         <Row className="d-flex text-start">
           <h3>Lavoro</h3>
