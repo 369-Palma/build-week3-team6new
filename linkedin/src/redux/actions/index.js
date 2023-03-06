384647e4889562ffe24acb4db5cdfa90458cccec
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-03-06 14:40:20 +0100

    .

--- linkedin/src/redux/actions/index.js
+++ linkedin/src/redux/actions/index.js
@@ -10,8 +10,6 @@ export const GET_DATA_LOADING_OFF = "GET_DATA_LOADING_OFF";
 const baseline = "https://striveschool-api.herokuapp.com/api/profile/";
 const postBaseline = "https://striveschool-api.herokuapp.com/api/posts/";
 
-const REACT_APP_API_KEY_COMMENT = process.env.REACT_APP_API_KEY_COMMENT;
-
 const REACT_APP_API_KEY_PALMA = process.env.REACT_APP_API_KEY_PALMA;
 /* const REACT_APP_API_KEY_GIACOMO = process.env.REACT_APP_API_KEY_GIACOMO;
 const REACT_APP_API_KEY_MICHELA = process.env.REACT_APP_API_KEY_MICHELA;
@@ -246,3 +244,4 @@ export const fetchPostsSearch = () => {
 };
 
 // FETCH COMMENTI
+const REACT_APP_API_KEY_COMMENT = process.env.REACT_APP_API_KEY_COMMENT;
