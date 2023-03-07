61e996c22008149d468938e0ccd2dae98dcf8c29
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-03-07 09:49:58 +0100

    prova collegamento profilo utente dinamico

--- linkedin/src/components/CardSidebar.jsx
+++ linkedin/src/components/CardSidebar.jsx
@@ -1,6 +1,7 @@
 import { Container, Button, Card, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { BsFillPersonPlusFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const CardSidebar = () => {
   const users = useSelector((state) => state.content.slice(0, 5));
@@ -25,10 +26,12 @@ const CardSidebar = () => {
                   <span className="text-muted fs-6 ms-1">• 3°+</span>
                 </Card.Title>
                 <Card.Text>{user.title}</Card.Text>
-                <Button className="my-2 bottoneSideBar">
-                  <BsFillPersonPlusFill className="me-2" />
-                  Collegati
-                </Button>
+                <Link className="nav-link" to="/profile/:userId">
+                  <Button className="my-2 bottoneSideBar">
+                    <BsFillPersonPlusFill className="me-2" />
+                    Collegati
+                  </Button>
+                </Link>
               </Card.Body>
             </Card>
           </Row>
