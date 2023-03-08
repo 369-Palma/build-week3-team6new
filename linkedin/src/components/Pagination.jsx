bc09436446fdd17d8cb334b2951342a350898c8b
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-03-08 14:29:15 +0100

    .

--- linkedin/src/components/Pagination.jsx
+++ linkedin/src/components/Pagination.jsx
@@ -1,22 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { useSelector } from "react-redux";
 import React, { useState } from "react";
-import { PaginationControl } from "react-bootstrap-pagination-control";
-const PaginationNews = () => {
-  const [page, setPage] = useState(1);
+/* import { PaginationControl } from "react-bootstrap-pagination-control";
+ */ import NewsFeed from "./NewsFeed";
 
-  return (
-    <PaginationControl
+const PaginationNews = () => {
+  const post = useSelector((state) => state.posts);
+  console.log(post);
+  const [page, setPage] = useState(1);
+  /* const [loading, setLoading] = useState(false); */
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(5);
+
+  const indexLastPost = currentPage * postsPerPage;
+  const indexFirstPost = indexLastPost - postsPerPage;
+  const currentPosts = post.slice(indexFirstPost, indexLastPost);
+
+  /* const pageCount = Math.ceil(post.length / postsPerPage);
+   */ return (
+    <NewsFeed>{currentPosts}</NewsFeed>
+    /*  <PaginationControl
       page={page}
       between={4}
-      total={250}
-      limit={20}
+      total={post.slice(0, 30)}
+      limit={10}
       changePage={(page) => {
         setPage(page);
         console.log(page);
       }}
       ellipsis={1}
-    />
+    /> */
   );
 };
 
