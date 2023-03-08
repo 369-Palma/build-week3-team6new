f1e0353ae5134a422b9a6143919cef1d268a86ec
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-03-08 10:25:03 +0100

    componente pagination da bootstrap

--- linkedin/src/components/NewsFeed.jsx
+++ linkedin/src/components/NewsFeed.jsx
@@ -2,20 +2,20 @@
 import { useEffect, useState } from "react";
 import { fetchPosts } from "../redux/actions/index";
 import { useSelector, useDispatch } from "react-redux";
-import { Button, Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row, Container } from "react-bootstrap";
 import { FiThumbsUp } from "react-icons/fi";
 import { BiCommentDetail, BiShare } from "react-icons/bi";
 import { FaTimes } from "react-icons/fa";
 import { fetchComm } from "../redux/actions";
-
+import PaginationNews from "./Pagination";
 
 import PostEditModal from "./PostEditModal";
 
 function NewsFeed() {
   const dispatch = useDispatch();
   const post = useSelector((state) => state.posts);
-  console.log(post)
-  const comm = useSelector((state) => state.comm)
+  console.log(post);
+  const comm = useSelector((state) => state.comm);
   const [image, setImage] = useState(null);
   const [formData, setformData] = useState(new FormData());
 
@@ -31,15 +31,12 @@ function NewsFeed() {
     dispatch(fetchPosts());
   }, []);
 
-
   useEffect(() => {
     dispatch(fetchComm(selectedPostId));
   }, [selectedPostId]);
 
-  console.log(post)
-  console.log(comm)
-
-
+  console.log(post);
+  console.log(comm);
 
   const handleImageChange = (ev) => {
     setformData((prev) => {
@@ -56,14 +53,17 @@ function NewsFeed() {
 
       formData.append("post", image);
 
-      let response = await fetch(`https://striveschool-api.herokuapp.com/api/posts/${postId}`, {
-        method: "POST",
-        body: formData,
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNzk0NGYxOTNlNjAwMTM4MDdmNWUiLCJpYXQiOjE2Nzc0OTA1MDAsImV4cCI6MTY3ODcwMDEwMH0.pf9G3SwntDHg3iUJZF-olKYGync7u8VErUGV_JFF91Y",
-        },
-      });
+      let response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/${postId}`,
+        {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNzk0NGYxOTNlNjAwMTM4MDdmNWUiLCJpYXQiOjE2Nzc0OTA1MDAsImV4cCI6MTY3ODcwMDEwMH0.pf9G3SwntDHg3iUJZF-olKYGync7u8VErUGV_JFF91Y",
+          },
+        }
+      );
       if (response.ok) {
         alert("Image uploaded!");
         setImage(null);
@@ -79,13 +79,16 @@ function NewsFeed() {
   // Fetch per eliminare un nostro post già creato precedentemente.
   const deletePost = async (postId) => {
     try {
-      let response = await fetch(`https://striveschool-api.herokuapp.com/api/posts/${postId}`, {
-        method: "DELETE",
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNzk0NGYxOTNlNjAwMTM4MDdmNWUiLCJpYXQiOjE2Nzc0OTA1MDAsImV4cCI6MTY3ODcwMDEwMH0.pf9G3SwntDHg3iUJZF-olKYGync7u8VErUGV_JFF91Y",
-        },
-      });
+      let response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/${postId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNzk0NGYxOTNlNjAwMTM4MDdmNWUiLCJpYXQiOjE2Nzc0OTA1MDAsImV4cCI6MTY3ODcwMDEwMH0.pf9G3SwntDHg3iUJZF-olKYGync7u8VErUGV_JFF91Y",
+          },
+        }
+      );
       if (response.ok) {
         alert("Post was deleted!");
       } else {
@@ -98,73 +101,94 @@ function NewsFeed() {
   };
 
   return (
-    <div className="d-flex justify-content-center">
-      <div style={{ width: "700px", maxWidth: "100%" }}>
-        {post.map((post) => (
-          <Card key={post?._id} style={{ margin: "1rem 0" }}>
-            <Card.Img variant="top" src={post?.user?.image} alt="foto" className="fotoTonde ms-2" />
-            <Card.Body>
-              <Card.Title>{post?.username} said:</Card.Title>
-              <Card.Text>{post?.text}</Card.Text>
-              {post?.image ? <Card.Img variant="bottom" src={post?.image} alt="fotopost" /> : null}
-            </Card.Body>
-            <hr className="my-1" />
-            <Row className="text-muted post-actions justify-content-center">
-              <Col xs="2" className="d-flex align-items-center justify-content-center p-2 mx-2 rounded">
-                <div className="mb-0 ml-2 text-primary">
-                  <FiThumbsUp /> Like
-                </div>
-              </Col>
-              <Col xs="2" className="d-flex align-items-center justify-content-center p-2 mx-2 rounded">
-                <div className="mb-0 ml-2">
-                  <Button
-                    // onClick={() => dispatch(fetchComm(post._id))}
-                    onClick={() => setSelectedPostId(post._id)}
+    <Container>
+      <Row className="d-flex justify-content-center">
+        <Col sm={10} style={{ width: "700px", maxWidth: "100%" }}>
+          {post.map((post) => (
+            <Card key={post?._id} style={{ margin: "1rem 0" }}>
+              <Card.Img
+                variant="top"
+                src={post?.user?.image}
+                alt="foto"
+                className="fotoTonde ms-2"
+              />
+              <Card.Body>
+                <Card.Title>{post?.username} said:</Card.Title>
+                <Card.Text>{post?.text}</Card.Text>
+                {post?.image ? (
+                  <Card.Img variant="bottom" src={post?.image} alt="fotopost" />
+                ) : null}
+              </Card.Body>
+              <hr className="my-1" />
+              <Row className="text-muted post-actions justify-content-center">
+                <Col
+                  xs="2"
+                  className="d-flex align-items-center justify-content-center p-2 mx-2 rounded"
+                >
+                  <div className="mb-0 ml-2 text-primary">
+                    <FiThumbsUp /> Like
+                  </div>
+                </Col>
+                <Col
+                  xs="2"
+                  className="d-flex align-items-center justify-content-center p-2 mx-2 rounded"
+                >
+                  <div className="mb-0 ml-2">
+                    <Button
+                      // onClick={() => dispatch(fetchComm(post._id))}
+                      onClick={() => setSelectedPostId(post._id)}
+                    >
+                      <BiCommentDetail /> Comment
+                    </Button>
+                  </div>
+                </Col>
+                <Col
+                  xs="2"
+                  className="d-flex align-items-center justify-content-center p-2 mx-2 rounded"
+                >
+                  <div className="mb-0 ml-2">
+                    <BiShare /> Share
+                  </div>
+                </Col>
+                {post?.user?._id === "63fc7944f193e60013807f5e" && (
+                  <>
+                    <PostEditModal post={post} onSave={handleSave} />
 
-                  >
-                    <BiCommentDetail /> Comment
-                  </Button>
-                </div>
-              </Col>
-              <Col xs="2" className="d-flex align-items-center justify-content-center p-2 mx-2 rounded">
-                <div className="mb-0 ml-2">
-                  <BiShare /> Share
-                </div>
-              </Col>
-              {post?.user?._id === "63fc7944f193e60013807f5e" && (
-                <>
-                  <PostEditModal post={post} onSave={handleSave} />
-
-                  <Col xs="2" className="d-flex align-items-center justify-content-center p-2 mx-2 rounded">
-                    <div className="text-danger" style={{ cursor: "pointer" }} onClick={() => deletePost(post._id)}>
-                      <FaTimes /> Delete
-                    </div>
-                  </Col>
-                </>
-              )}
-            </Row>
-            <Row>
-
-              {selectedPostId === post._id && (
-                <Card className="mt-3">
-                  <Card.Body>
-                    {comm
-                      .map((com) => (
+                    <Col
+                      xs="2"
+                      className="d-flex align-items-center justify-content-center p-2 mx-2 rounded"
+                    >
+                      <div
+                        className="text-danger"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => deletePost(post._id)}
+                      >
+                        <FaTimes /> Delete
+                      </div>
+                    </Col>
+                  </>
+                )}
+              </Row>
+              <Row>
+                {selectedPostId === post._id && (
+                  <Card className="mt-3">
+                    <Card.Body>
+                      {comm.map((com) => (
                         <div key={com._id} className="mb-2">
                           <p className="mb-0">{com.comment}</p>
                           <small className="text-muted">{com.author}</small>
                         </div>
                       ))}
-                  </Card.Body>
-                </Card>
-              )}
-            </Row>
-
-          </Card>
-        ))
-        }
-      </div>
-    </div>
+                    </Card.Body>
+                  </Card>
+                )}
+              </Row>
+            </Card>
+          ))}
+        </Col>
+      </Row>
+      <PaginationNews></PaginationNews>
+    </Container>
   );
 }
 
