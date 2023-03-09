803960b5aa4a2463452033de5737a0389ab79449
Giacomo <giax.95@live.it>
2023-03-09 09:56:02 +0100

    A

--- linkedin/src/components/Home.jsx
+++ linkedin/src/components/Home.jsx
@@ -1,13 +1,13 @@
 import NewsFeed from "./NewsFeed";
 /* import Searchbar from "./Searchbar"; */
 import StartAPost from "./CreatePostNewspage";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { Spinner, Container, Row, Col } from "react-bootstrap";
 import HomeLeftSidebar from "./HomeLeftSidebar";
 import HomeRightSidebar from "./HomeRightSidebar";
 
 const Home = () => {
-  const isLoading = useSelector((state) => state.isLoading);
+  // const isLoading = useSelector((state) => state.isLoading);
 
   return (
     <>
@@ -20,7 +20,7 @@ const Home = () => {
             <StartAPost />
 
             {/* <Searchbar /> */}
-            {isLoading && <Spinner animation="border" variant="primary" className="my-2" />}
+            {/* {isLoading && <Spinner animation="border" variant="primary" className="my-2" />} */}
             <NewsFeed />
           </Col>
           <Col xs={3} className="order-3 mt-2">
