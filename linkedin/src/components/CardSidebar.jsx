03b5d4c7d25df1a9bd17ad2d3a822b3d546bd95c
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-03-10 14:18:49 +0100

    filter per vedere le immagini degli users nella sidebar

--- linkedin/src/components/CardSidebar.jsx
+++ linkedin/src/components/CardSidebar.jsx
@@ -7,26 +7,36 @@ const CardSidebar = ({ users }) => {
   console.log(users);
   return (
     <Container>
-      {users.map((user) => (
-        <Row key={user._id} className="d-flex flex-row align-items-center px-2">
-          <Card className="cardImgsideBar d-flex flex-row mt-1">
-            <Card.Img variant="top" src={user.image} alt="foto" className="fotoTonde align-self-start" />
-            <Card.Body className="flex-column">
-              <Card.Title className="ms-5">
-                <Link to={`/profile/${user._id}`} className="link-sidebar">
-                  {user.name} {user.surname}
-                </Link>
-                <span className="text-muted fs-6 ms-1">• 3°+</span>
-              </Card.Title>
-              <Card.Text className="ms-5">{user.title}</Card.Text>
-              <Button className="my-2 bottoneSideBar">
-                <BsFillPersonPlusFill className="me-2" />
-                Collegati
-              </Button>
-            </Card.Body>
-          </Card>
-        </Row>
-      ))}
+      {users
+        .filter((user) => user.image !== "")
+        .map((user) => (
+          <Row
+            key={user._id}
+            className="d-flex flex-row align-items-center px-2"
+          >
+            <Card className="cardImgsideBar d-flex flex-row mt-1">
+              <Card.Img
+                variant="top"
+                src={user.image}
+                alt="foto"
+                className="fotoTonde align-self-start"
+              />
+              <Card.Body className="flex-column">
+                <Card.Title className="ms-5">
+                  <Link to={`/profile/${user._id}`} className="link-sidebar">
+                    {user.name} {user.surname}
+                  </Link>
+                  <span className="text-muted fs-6 ms-1">• 3°+</span>
+                </Card.Title>
+                <Card.Text className="ms-5">{user.title}</Card.Text>
+                <Button className="my-2 bottoneSideBar">
+                  <BsFillPersonPlusFill className="me-2" />
+                  Collegati
+                </Button>
+              </Card.Body>
+            </Card>
+          </Row>
+        ))}
     </Container>
   );
 };
